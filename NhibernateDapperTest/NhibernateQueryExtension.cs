2ba956c198c65cb1b5e203b78bfa06d1a2f9db93
Азат Мухамадьяров <a.mukhamadiarov@bars-open.ru>
2023-02-06 14:26:17 +0300

    методы расширения для NhibernateQuery

---
+++ NhibernateDapperTest/NhibernateQueryExtension.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using NHibernate;
+
+namespace NhibernateDapperTest;
+
+public static class NhibernateQueryExtension
+{
+    /// <summary>
+    /// Установить параметры для запроса
+    /// </summary>
+    /// <param name="query">Запрос</param>
+    /// <param name="parameters">Словарь параметров в виде наименование(ключ) - значение</param>
+    public static IQuery SetParameters(this IQuery query, Dictionary<string, object> parameters)
+    {
+        foreach (var (key, value) in parameters)
+        {
+            if (value is ICollection enumerable)
+            {
+                query.SetParameterList(key, enumerable);
+            }
+            else
+            {
+                query.SetParameter(key, value);					
+            }
+        }
+
+        return query;
+    }
+		
+    /// <summary>
+    /// Установить параметры для запроса
+    /// </summary>
+    /// <param name="query">Запрос</param>
+    /// <param name="parameters">Параметры в виде свойств объекта (примерно как в Dapper)</param>
+    public static IQuery SetParameters(this IQuery query, object parameters)
+    {
+        var properties = parameters.GetType().GetProperties();
+
+        foreach (var property in properties)
+        {
+            var value = property.GetValue(parameters);
+            if (value is ICollection enumerable)
+            {
+                query.SetParameterList(property.Name, enumerable);
+            }
+            else
+            {
+                query.SetParameter(property.Name, value);					
+            }
+        }
+			
+        return query;
+    } 
+}
